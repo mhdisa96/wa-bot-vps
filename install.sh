57e8da4e14841072e102e80d531b33e4ad3cee05
Muhammad Isa <162756570+mhdisa96@users.noreply.github.com>
2026-04-05 15:37:52 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "🚀 INSTALL BOT WA VPS"
+
+apt update -y
+apt install -y curl git nodejs npm
+
+echo "📥 CLONE REPO"
+git clone https://github.com/mhdisa96/wa-bot-vps.git
+cd wa-bot-vps
+
+echo "📦 INSTALL MODULE"
+npm install
+
+echo "⚙️ SET ENV"
+cp .env.example .env
+
+echo "📡 INSTALL PM2"
+npm install -g pm2
+
+echo "▶️ START BOT"
+pm2 start index.js --name wa-bot
+pm2 save
+
+echo "✅ SELESAI"
